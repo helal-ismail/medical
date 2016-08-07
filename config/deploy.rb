db152e1ddeda9c8ed6e05d851e31b456da9bb8e9
Helal Ismail <helal.hamed@gmail.com>
2016-08-07 16:53:14 +0300

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -74,8 +74,9 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      Rake::Task["puma:restart"].reenable
       invoke 'puma:restart'
+      Rake::Task["puma:restart"].reenable
+      
     end
   end
 
