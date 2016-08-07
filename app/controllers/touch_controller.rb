04299f0abc6fbe4b03d609d661b86817a5c4e12d
Helal Ismail <helal.hamed@gmail.com>
2016-08-07 16:40:58 +0300

    added exception handler

--- app/controllers/touch_controller.rb
+++ app/controllers/touch_controller.rb
@@ -1,5 +1,10 @@
 class TouchController < ApplicationController
   def touch
+    begin
     render :json => {:success => true, :msg => "Server Online", :timestamp => "#{DateTime.now}"}
+    rescue Exception => e
+      render :json => {:success => false, :msg => "#{e.message}"}
+    end
+      
   end
 end
