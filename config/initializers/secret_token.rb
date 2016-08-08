257d4c58f81870afbe5226e24d6ebd8d7aeea6f8
Helal Ismail <helal.hamed@gmail.com>
2016-08-08 11:40:51 +0300

    fix app name

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Backend::Application.config.secret_key_base = 'd18c848425321cc091bfa4fe62f3f2895e6f8511e524994f174e8f5dae8870e9da3007a1e955d8e70c8e48658844f7c61eb
+Medical::Application.config.secret_key_base = 'd18c848425321cc091bfa4fe62f3f2895e6f8511e524994f174e8f5dae8870e9da3007a1e955d8e70c8e48658844f7c61eb
 a5e0b8d4bd59812930f89ac615124'
