3207f463f7d363357eb07b1e6f1256ee36d51cb6
HH <--global>
2016-08-21 20:58:40 +0300

    user functions

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,3 +1,18 @@
 class Api::UsersController < ApiController
+  
+  def register
+    
+  end
 
+  def login
+    
+  end
+  
+  def edit_profile
+    
+  end
+  
+  def get_profile
+    
+  end
 end
