683fe990b4b29f1b96120d4268ef4fa1d3a347a6
HH <--global>
2016-08-21 21:36:28 +0300

    fixed associations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,7 +1,7 @@
 class Appointment < ActiveRecord::Base
   belongs_to :patient
   belongs_to :doctor_prices
-  belongs_to :doctor, :through => :doctor_prices
-  belongs_to :clinic, :through => :doctor_prices
+#  belongs_to :doctor, :through => :doctor_prices
+#  belongs_to :clinic, :through => :doctor_prices
   
 end
--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -1,5 +1,5 @@
 class Clinic < ActiveRecord::Base
-  has_and_belongs_to_many :doctors, :through => :doctor_prices
+  has_many :doctors, :through => :doctor_prices
   belongs_to :hospital
   belongs_to :specialization
   has_many :appointments, :through => :doctor_prices
