9d97f704b0c4b8758e00fda220a9f03af47c06dc
HH <--global>
2016-09-07 00:11:52 +0300

    Schedule APIs

--- app/controllers/api/doctors_controller.rb
+++ app/controllers/api/doctors_controller.rb
@@ -3,32 +3,42 @@ class Api::DoctorsController < ApiController
   api :GET, '/doctor', "Get Doctor by ID"
   param :doctor_id, String, :desc => "Doctor ID", :required => true
   def get_doctor
-    
+
   end
-  
+
   api :GET, '/doctors', "Get a List of Doctors by a Specific Query"
+
   def get_doctors
-    
+
   end
-  
+
   api :POST, '/doctor/feedback', "Leave Feedback to Doctor"
   param :doctor_id, String, :desc => "Doctor ID", :required => true
   param :feedback, String, :desc => "Text Feedback", :required => true
   param :stars, Integer, :desc => "Stars count [ 1 (Worst) - 5 (Best) ]", :required => true
+
   def add_feedback
-    
+
   end
-  
+
   api :POST, '/doctor/schedule', "Set Doctor's Schedule"
   param :doctor_id, String, :desc => "Doctor ID", :required => true
   param :schedule, Array, :desc => "JSON Array of weekdays / schedule", :required => true
+
   def set_schedule
-    
+    doctor_price = DoctorPrice.find_by_doctor_and_clinic(params[:doctor_id], params[:clinic_id])
+    schedule = JSON.parse params[:schedule].downcase
+    schedule.each do |schedule_json|
+      add_daily_schedule(schedule_json)
+    end
+    render :json => {:success => true, :message => "Schedule has been updated"}
   end
-  
+
   api :GET, '/doctor/schedule', "Get Doctor's Schedule"
   param :doctor_id, String, :desc => "Doctor ID", :required => true
+  param :clinic_id, String, :desc => "Clinic ID", :required => true
   def get_schedule
-    
+    doctor_price = DoctorPrice.find_by_doctor_and_clinic(params[:doctor_id], params[:clinic_id])
+    render :json => {:success => true, :data => doctor_price.daily_schedule}
   end
 end
--- app/models/doctor_price.rb
+++ app/models/doctor_price.rb
@@ -4,6 +4,19 @@ class DoctorPrice < ActiveRecord::Base
   has_many :daily_schedules
   has_many :appointments
 
+def self.find_by_doctor_and_clinic(doctor_id, clinic_id)
+  doctor_price = DoctorPrice.where(:doctor_id=>doctor_id, :clinic_id=>clinic_id).first
+  doctor_price
+end
+
+def add_daily_schedule(schedule_json)
+  schedule = DailySchedule.new
+  schedule.day_of_week = schedule_json[:day_of_week]
+  schedule.start_time = schedule_json[:start_time]
+  schedule.end_time = schedule_json[:end_time]
+  schedule.doctor_price = self
+  schedule.save
+end
 
 def get_next_appointment_num
   appt = self.appointments.last
