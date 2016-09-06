73e89807e25f97a7abffafe22b457bf9741c8a2d
HH <--global>
2016-09-07 00:14:15 +0300

    GET PATIENT BY ID

--- app/controllers/api/patients_controller.rb
+++ app/controllers/api/patients_controller.rb
@@ -3,7 +3,8 @@ class Api::PatientsController < ApiController
   api :GET, '/patient', "Get Patient by ID"
   param :patient_id, String, :desc => "Patient ID", :required => true
   def get_patient
-    
+    patiend = Patient.find(params[:id])
+    render :json => {:success=>true, :data=>patient}
   end
   
   api :GET, '/patients', "Get a List of Patients by a Specific Query"
