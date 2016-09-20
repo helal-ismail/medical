53352ad86bf8e0ec598029679cccb99b7f256557
HH <--global>
2016-09-20 11:36:37 +0300

    handle exception

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,9 @@
 class ApiController < ApplicationController
+  
+  rescue_from ActionController::Excepton, :with => :handle_exception
+
+  def handle_exception(exception)
+    render :json => {:success=>false, :msg=>exception.message }, :status => 500
+  end
 
 end
