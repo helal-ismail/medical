171ef75245610b5c1649c794362cf11ffb71370a
HH <--global>
2016-09-20 11:23:21 +0300

    Login & Register API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   apipie
   root 'touch#touch'
+  
+  namespace :api do
+    post 'user' , to: 'users#register'
+    post 'user/login' , to: 'users#login'
+    
+  end
 
 
 end
