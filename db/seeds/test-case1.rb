2e494f0f26696b5b15fa1171e7cb8aab184e79a7
HH <--global>
2016-12-15 00:46:02 +0200

    added more hospitals to seed file

--- db/seeds/test-case1.rb
+++ db/seeds/test-case1.rb
@@ -7,6 +7,8 @@ password = "123456"
 # Hospitals
 Hospital.create(name: 'German Hospital', address: 'Qesm AR Ramel سابا محطة باشا, Fleming, Qism El-Raml, Alexandria Governorate', uid: '1', phone: '123123123', website: 'www.hospital.com', email: 'contact@hospital.com')
 Hospital.create(name: 'Louran Hospital', address: 'St. - Louran, 13 Shaarawy, San Stifano, Qesm AR Ramel, Alexandria Governorate', uid: '2', phone: '123123123', website: 'www.hospital.com', email: 'contact@hospital.com')
+Hospital.create(name: 'Andalusia Hospital', address: '35 Bahaa Eldeen ElGhatoury street, Alexandria, Egypt', uid: '3', phone: '123123123', website: 'www.hospital.com', email: 'contact@hospital.com')
+Hospital.create(name: 'ICC Hospital', address: '24 Bahaa Eldeen ElGhatoury street, Alexandria, Egypt', uid: '4', phone: '123123123', website: 'www.hospital.com', email: 'contact@hospital.com')
 
 spec_names = ["أمراض القلب","أمراض الباطنية","العيون والابصار","الأوعية الدموية"]
 specs = []
