1615ae24349f593d0415d1bc1218ea38ae2c8f85
HH <--global>
2016-12-15 19:23:25 +0200

    changed releases size

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,8 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :keep_releases, 3
+
 #set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
 ## Defaults:
