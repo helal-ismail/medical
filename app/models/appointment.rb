3352df77cc9fad05f8b042dfbb025835ee893528
HH <--global>
2016-12-19 12:41:03 +0200

    added doctor desc

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -48,7 +48,7 @@ class Appointment < ActiveRecord::Base
         result = {:id => self.id, :discount => self.discount, :price => self.price, :state => self.state,
                   :appointment_date => self.appointment_date, :appointment_time => self.appointment_time.strftime('%r'),
                   :patient_id => self.patient.id, :patient_name => self.patient.name,
-                  :doctor_id => self.doctor_price.doctor.id, :doctor_name => self.doctor_price.doctor.name,
+                  :doctor_id => self.doctor_price.doctor.id, :doctor_name => self.doctor_price.doctor.name, :doctor_description => self.doctor_price.doctor.description
                   :notes => self.notes || '', :clinic_id => self.doctor_price.clinic.id, :clinic_name => self.doctor_price.clinic.name, :specialization => self.doctor_price.clinic.specialization.name }
   end
 
