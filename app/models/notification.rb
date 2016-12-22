10c0a679b098f46cf674067a293270c5cec8cb13
HH <--global>
2016-12-22 16:23:02 +0200

    notf

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,5 +2,9 @@ class Notification < ActiveRecord::Base
   belongs_to :user
   enum state: [ :new, :seen]
   
+  def as_json(options)
+    result = {:id => self.id, :title => self.title, :description => self.description, :state => self.state }
+    result
+  end
   
 end
