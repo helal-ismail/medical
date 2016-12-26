2d22915d6e9c15159d923392880e592f2aeb237b
yehia-elsaka <yahya.elsaka@gmail.com>
2016-12-26 18:25:50 +0200

    change notification states sequance

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -7,8 +7,8 @@ class Api::NotificationsController < ApiController
   def user_notifications
     user = User.find(params[:user_id])
     if user.present?
-      counter = user.notifications.where(:state => 0).count
-      Notification.set_pending_to_delivered
+      counter = user.notifications.where(:state => 1).count
+      Notification.set_delivered_to_pending
       render :json => {:data => user.notifications.reverse, :pending => counter}
     else
       render :json => {:msg => "User not found"}, :status => 400
