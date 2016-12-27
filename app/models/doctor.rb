d2ac27376fd92a5f2c3b847e8072c7f92223a8c4
yehia-elsaka <yahya.elsaka@gmail.com>
2016-12-27 11:55:54 +0200

    edit doctor as_json to return feedback

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,7 @@ class Doctor < User
   has_many :doctor_prices
   has_many :clinics, :through => :doctor_prices
   has_many :appointments, :through => :doctor_prices
+  has_many :feedbacks
 
   def self.search_by_pattern(pattern)
     if pattern.blank?  # blank? covers both nil and empty string
@@ -14,7 +15,7 @@ class Doctor < User
   def as_json(options)
     #super(:only => [:id, :uid, :name])
     result = {:id => self.id, :name => self.name, :description => self.description || ''}
-    
+
     if options[:detailed_info].present?
       clinics_result = []
       self.clinics.each do |clinic|
@@ -26,6 +27,22 @@ class Doctor < User
         clinics_result << sub_result
       end
       result[:clinics] = clinics_result
+
+      feedbacks = Feedback.get_feedback("Doctor", self.id)
+
+      if feedbacks.present?
+        rating = 0
+        feedbacks.each do |f|
+          rating += f.stars
+        end
+
+        rating = rating.to_f / feedbacks.length
+        rating = number_with_precision(rating, :precision => 2)
+
+        result[:feedbacks] = feedbacks
+        result[:total_rate] = rating
+      end
+
     end
     return result
   end
