58afd6ab6f5eb2fe396460a5a4001c2750d2f30e
yehia-elsaka <yahya.elsaka@gmail.com>
2016-12-28 16:07:02 +0200

    edit staging server ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,4 @@
-server '', roles: [:web, :app, :db], primary: true
+server '45.55.246.135', roles: [:web, :app, :db], primary: true
 set :stage,           :staging
 set :branch,        :development
 set :rails_env, 'staging'
-
