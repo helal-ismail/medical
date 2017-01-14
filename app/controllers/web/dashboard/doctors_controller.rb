f97f36b912ff522ebbc56e5f2a4b5e95c2baa775
HH <--global>
2017-01-15 00:07:41 +0200

    integration

--- app/controllers/web/dashboard/doctors_controller.rb
+++ app/controllers/web/dashboard/doctors_controller.rb
@@ -1,6 +1,23 @@
 class Web::Dashboard::DoctorsController < Web::DashboardController
 
   def index
-
+    url = request.path_info
+    @doctors = []
+    @clinics = []
+    if url.include?('hospitals')
+      hospital = Hospital.find(params[:id])
+      @clinics = hospital.clinics
+      @clinics.each do |clinic|
+        @doctors = @doctors + clinic.doctors
+      end
+    elsif url.include?('clinics')
+      clinic = Clinic.find(params[:id])
+      @clinics << clinic
+      @doctors = clinic.doctors
+    else
+      @doctors = Doctor.all
+      #@clinics = Clinic.private_clinics
+    end
   end
+
 end
