7d0f6181e4d2f4eb797684d59209069eb913997f
HH <--global>
2017-01-18 16:06:18 +0200

    appt time

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Medical
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   #  config.assets.initialize_on_precompile = false
-    #Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+    Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
 
   end
 end
